2b29eafbc9ede6e2f1f575eba6c06b85112e456f
Kim Tran <45058415+mikim190@users.noreply.github.com>
2019-06-18 16:36:20 -0700

    Update

--- server/index.js
+++ server/index.js
@@ -1,27 +1,41 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
+const request = require('request');
 const app = express();
 const PORT = 1111;
 
-app.use(express.static(path.join(__dirname, '../public')))
+
+app.use(express.static(path.join(__dirname, '../public/')))
 
 app.get('/restaurants/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '/../public/index.html'));
 });
 
-app.get('/restaurants/:id/info', (req, res) => {
-  res.redirect(`http://127.0.0.1:3002/api/restaurants/${req.params.id}/info`);
+app.get('/api/restaurants/:id/info', (req,res) => {
+  
+  const { id } = req.params;
+  request('http://127.0.0.1:3005/api/restaurants/' + id + '/info', (err, results) => {
+  if (err) {
+    res.send(err);
+  }
+  })
+  .pipe(res)
 });
 
-app.get('/restaurants/:id/reviews', (req, res) => {
-  res.redirect(`http://127.0.0.1:3001/api/restaurants/${req.params.id}/reviews`);
-});
+// app.get('/restaurants/:id/reviews', (req, res) => {
+//   res.redirect(`http://127.0.0.1:3001/api/restaurants/${req.params.id}/reviews`);
+// });
 
-app.get('/restaurants/:id/photos', (req, res) => {
-  res.redirect(`http://127.0.0.1:3000/api/restaurants/${req.params.id}/photos`);
-});
+// app.get('/restaurants/:id/photos', (req, res) => {
+//   res.redirect(`http://127.0.0.1:3000/api/restaurants/${req.params.id}/photos`);
+// });
 
-app.get('/restaurants/:id/googlereviews', (req, res) => {
-  res.redirect(`http://127.0.0.1:3003/api/restaurants/${req.params.id}/googlereviews`);
-});
+// app.get('/restaurants/:id/googlereviews', (req, res) => {
+//   res.redirect(`http://127.0.0.1:3003/api/restaurants/${req.params.id}/googlereviews`);
+// });
+
 app.listen(PORT, () => console.log(`Proxy server listening on port ${PORT}!`));
+
+
+
